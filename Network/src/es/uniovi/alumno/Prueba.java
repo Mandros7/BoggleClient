bab17adbbf4abd95b732018ed82f2eb6d0daf635
Hector Rodriguez <uo223411@uniovi.es>
2014-03-18 17:21:40 +0100

    Añadida una prueba de la clase Network

---
+++ Network/src/es/uniovi/alumno/Prueba.java
@@ -0,0 +1,12 @@
+package es.uniovi.alumno;
+import es.uniovi.computadores.Network;
+
+public class Prueba {
+
+	public static void main(String[] args) {
+		Network red = new Network();
+		System.out.println("Funcionando");
+		red.close();
+	}
+
+}
