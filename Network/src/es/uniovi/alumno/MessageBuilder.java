4c4c1dc1c5d2f34a75c5c2752fbbc06fe8d914ad
hectorrcov <hectorrcov@localhost>
2014-05-05 00:10:39 +0200

    Se corrige excepcion para palabras de menos de 3 letras al meter comando /WORD. Se pasa el metodo switch que decide que mensaje enviar a la clase MessageBuilder

--- Network/src/es/uniovi/alumno/MessageBuilder.java
+++ Network/src/es/uniovi/alumno/MessageBuilder.java
@@ -12,6 +12,107 @@ public class MessageBuilder {
 		this.OutBuf = abq;
 	}
 	
+	
+	void newInput(String input){
+		if (input.length()>2){
+      	  String inputFixed = input.replaceAll(" +", " ").trim();
+      	  //Sustuye los espacios si son 2 o mas seguidos por uno unico y elimina espacios al principio y al final.
+            String [] data = inputFixed.split(" ");             
+            if (inputFixed.charAt(0)=='/'){
+            	data[0] = data[0].toUpperCase();
+                switch (data[0]) {
+                    case ("/START"):		                   
+                          startGame();
+							break;
+                    case ("/NICK"):
+                    	if (data.length!=2) {
+                    		System.out.println("Numero de arumentos incorrecto.\n" +
+                    				"Formato: /nick <nick>");
+                    	}
+                    	else {
+	                    		changeNick(data[1]);
+                    	}
+                    	break;
+                    case ("/JOIN"):
+                    	if (data.length!=2) {
+                    		System.out.println("Numero de arumentos incorrecto.\n" +
+                    				"Formato: /join <mesa>");
+                    	}
+                    	else {
+	                    		joinTable(data[1]);
+                    	}
+                    	break;
+                    	
+                    case ("/LEAVE"):
+                    		leaveTable();
+                    	break;
+                    	
+                    case ("/MATRIX"):
+                    	if (Client.getMATRIX() != null){
+                    		Character[][] MATRIX = Client.getMATRIX();
+	                        	System.out.println("------------");
+	                        	for (int i=0; i<MATRIX.length;i++){
+									String line = "";
+									for (int j = 0; j<MATRIX.length;j++){
+										line = line + (MATRIX[i][j]);
+										if (j!=MATRIX.length-1){
+											line = line + ", ";
+										}
+									}
+									System.out.println(line);
+								}
+								System.out.println("------------");
+                    	}
+                    	else {
+                    		System.out.println("No se ha recibido matriz del servidor");
+                    	}
+                    	break;
+                    	
+                    case ("/LIST"):
+                		listTables();
+                    	break;
+                    	
+                    case ("/WHO"):
+                		whoTable();
+                    	break;
+                    	
+                    case ("/QUIT"):
+                  	System.out.println("Hasta luego.");
+                    	break;
+                    	
+                    case ("/WORD"):
+                    if (data.length>1){
+	                        if (data.length>2){
+	                        	System.out.println("Solo se reconoce "+data[1]+" como palabra");	                 
+	                        }
+	                        if (data[1].length()>2){
+	                        	sendWord(data[1]);
+	                        }
+	                        else {
+	                        	System.out.println("La longitud minima de la palabra es 3 caracteres");
+	                        }
+	                
+                    	}
+                    	else {
+                    		System.out.println("Debes escribir una palabra.\n" +
+                    				"Formato: /word <palabra>");
+                    	}
+                    	break;
+                    default:
+                    	System.out.println("Has introducido un comando incorrecto.");
+                        break;
+                }
+            }
+            else{
+                    sendWord(data[0]);
+            }
+        }
+        else {
+      	  System.out.println("Longitud minima: 3 caracteres.");
+        }
+	}
+	
+	
 	void startGame(){
 		STARTCommandMessage start = new STARTCommandMessage();
 		try {
